a40018ed925ba3af0c87e6e95fc9d7f83a382740
João Lacerda <joao.lacerda0@gmail.com>
2020-04-15 20:33:48 -0300

    main

--- src/main/java/template/atividade/AppTemplateAtividade.java
+++ src/main/java/template/atividade/AppTemplateAtividade.java
@@ -2,6 +2,10 @@ package template.atividade;
 
 public class AppTemplateAtividade {
     public static void main(String[] args) {
+        CompraLojaVirtual compraVirtual = new CompraLojaVirtual();
+        compraVirtual.processarCompra();
 
+        CompraLojaFisica compraLojaFisica = new CompraLojaFisica();
+        compraLojaFisica.processarCompra();
     }
 }
